6e1ba686c369e1d3e235750e41fce1e8d388cccc
Reuben <grerlich@gmail.com>
2024-10-28 12:12:31 -0400

    updated navbar to render differently on different pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 // src/components/NavBar.jsx
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { LogOut } from 'lucide-react'; // This is just for the icon
 import '../styles/NavBar.css';
@@ -8,6 +8,7 @@ import '../styles/NavBar.css';
 const NavBar = () => {
     const { signout, user } = useAuth(); // We only need signout from auth
     const navigate = useNavigate();
+    const location = useLocation();
 
     const handleLogout = () => {
         signout(); // Use the signout function from AuthContext
@@ -18,8 +19,14 @@ const NavBar = () => {
         <nav className="navbar">
             {user ? (
                 <div className="nav-links">
-                    <Link to="/landing">Home</Link>
-                    <Link to="/profile">Profile</Link>
+                    { location.pathname === "/landing" ? (
+                        <Link to="/profile">Profile</Link>
+                    ) : location.pathname === "/profile" ?  <Link to="/landing">Home</Link> : (
+                        <>
+                            <Link to="/profile">Profile</Link>
+                            <Link to="/landing">Home</Link>
+                        </>
+                    )}
                     <button onClick={handleLogout} className="logout-button">
                         <LogOut size={18} /> {/* Use LogOut as an icon component */}
                         <span>Logout</span>
