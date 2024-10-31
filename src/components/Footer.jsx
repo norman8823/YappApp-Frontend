234414276466332e584d812e5a46210550c6cc3a
Rani Bungay <ranibungay@gmail.com>
2024-10-31 09:23:51 -0400

    create aboutme and faq

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
     <nav className="footer">
       <div className="nav-links">
         <Link to="/team">Meet the Team</Link>
+        <Link to="/FAQ">How To Yap</Link>
       </div>
     </nav>
   );
